f54cd8bd36811a6e4f8e5031732096caadd0834a
wangqingyu-dev <62085025+wangqingyu-dev@users.noreply.github.com>
2021-08-05 10:57:45 +0800

    读取文件内容

--- src/main/java/com/rt/base/mytest/util/DevFileUtil.java
+++ src/main/java/com/rt/base/mytest/util/DevFileUtil.java
@@ -24,7 +24,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DevFileUtil {
-
+    public static String readFileContent(String fileName) {
+        File file = new File(fileName);
+        BufferedReader reader = null;
+        StringBuffer sbf = new StringBuffer();
+        try {
+            reader = new BufferedReader(new FileReader(file));
+            String tempStr;
+            while ((tempStr = reader.readLine()) != null) {
+                sbf.append(tempStr).append("\n");
+            }
+            reader.close();
+            return sbf.toString();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+        return sbf.toString();
+    }
     /**
      * 读取 Excel -> workbook
      *
